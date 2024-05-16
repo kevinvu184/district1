069d138731a3ba396c26a8f691efff4f8ecb5a2d
Kevin Vu <kevinvu184@gmail.com>
2024-05-17 09:44:07 +1000

    Rename

--- apps/up-companion/vite.config.ts
+++ apps/up-companion/vite.config.ts
@@ -34,9 +34,7 @@ export default defineConfig({
 
   test: {
     globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest',
-    },
+
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 
