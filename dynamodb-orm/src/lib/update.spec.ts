44c25ee5a78780131d5ce38c326d8c7a1a539592
Kevin Vu <kevinvu184@gmail.com>
2024-05-17 20:42:06 +1000

    Add types

--- dynamodb-orm/src/lib/update.spec.ts
+++ dynamodb-orm/src/lib/update.spec.ts
@@ -1,10 +1,53 @@
 import { update } from './update';
 
-it('return an update command input with key and table name', () => {
+it('return an undefined update command input', () => {
+  const received = update();
+  const expected = { Key: undefined, TableName: undefined };
+  expect(received).toStrictEqual(expected);
+});
+
+it('return a minimal update command input', () => {
   const received = update();
   const expected = {
-    Key: { PK: { S: '123' } },
-    TableName: 'table-1',
+    Key: {
+      Artist: {
+        S: 'Acme Band',
+      },
+      SongTitle: {
+        S: 'Happy Day',
+      },
+    },
+    TableName: 'Music',
+  };
+  expect(received).toStrictEqual(expected);
+});
+
+it('return a simple update command input', () => {
+  const received = update();
+  const expected = {
+    ExpressionAttributeNames: {
+      '#AT': 'AlbumTitle',
+      '#Y': 'Year',
+    },
+    ExpressionAttributeValues: {
+      ':t': {
+        S: 'Louder Than Ever',
+      },
+      ':y': {
+        N: '2015',
+      },
+    },
+    Key: {
+      Artist: {
+        S: 'Acme Band',
+      },
+      SongTitle: {
+        S: 'Happy Day',
+      },
+    },
+    ReturnValues: 'ALL_NEW',
+    TableName: 'Music',
+    UpdateExpression: 'SET #Y = :y, #AT = :t',
   };
   expect(received).toStrictEqual(expected);
 });
--- dynamodb-orm/src/lib/update.ts
+++ dynamodb-orm/src/lib/update.ts
@@ -1,8 +1,82 @@
-import { UpdateItemCommandInput } from '@aws-sdk/client-dynamodb';
+type AttributeAction = 'ADD' | 'DELETE' | 'PUT';
 
-export const update = (): UpdateItemCommandInput => {
-  return {
-    Key: { PK: { S: '123' } },
-    TableName: 'table-1',
+type ReturnConsumedCapacity = 'INDEXES' | 'NONE' | 'TOTAL';
+
+type ReturnItemCollectionMetrics = 'NONE' | 'SIZE';
+
+type ReturnValue =
+  | 'ALL_NEW'
+  | 'ALL_OLD'
+  | 'NONE'
+  | 'UPDATED_NEW'
+  | 'UPDATED_OLD';
+
+type ReturnValuesOnConditionCheckFailure = 'ALL_OLD' | 'NONE';
+
+type ConditionalOperator = 'AND' | 'OR';
+
+type ComparisonOperator =
+  | 'BEGINS_WITH'
+  | 'BETWEEN'
+  | 'CONTAINS'
+  | 'EQ'
+  | 'GE'
+  | 'GT'
+  | 'IN'
+  | 'LE'
+  | 'LT'
+  | 'NE'
+  | 'NOT_CONTAINS'
+  | 'NOT_NULL'
+  | 'NULL';
+
+type AttributeValue =
+  | { B: Uint8Array } // "B": "dGhpcyB0ZXh0IGlzIGJhc2U2NC1lbmNvZGVk"
+  | { BOOL: boolean } // "BOOL": true
+  | { BS: Uint8Array[] } //"BS": ["U3Vubnk=", "UmFpbnk=", "U25vd3k="]
+  | { L: AttributeValue[] } // "L": [ \{"S": "Cookies"\} , \{"S": "Coffee"\}, \{"N": "3.14159"\}]
+  | { M: Record<string, AttributeValue> } // "M": \{"Name": \{"S": "Joe"\}, "Age": \{"N": "35"\}\}
+  | { N: string } // "N": "123.45"
+  | { NS: string[] } // "NS": ["42.2", "-19", "7.5", "3.14"]
+  | { NULL: boolean } // "NULL": true
+  | { S: string } // "S": "Hello"
+  | { SS: string[] }; // "SS": ["Giraffe", "Hippo" ,"Zebra"]
+
+type AttributeValueUpdate = {
+  Action?: AttributeAction;
+  Value?: AttributeValue;
+};
+
+type ExpectedAttributeValue = {
+  AttributeValueList?: AttributeValue[];
+  ComparisonOperator?: ComparisonOperator;
+  Exists?: boolean;
+  Value?: AttributeValue;
+};
+
+type UpdateItemInput = {
+  // Required
+  Key: Record<string, AttributeValue> | undefined;
+  TableName: string | undefined;
+
+  // Optional
+  AttributeUpdates?: Record<string, AttributeValueUpdate>;
+  ConditionalOperator?: ConditionalOperator;
+  ConditionExpression?: string;
+  Expected?: Record<string, ExpectedAttributeValue>;
+  ExpressionAttributeNames?: Record<string, string>;
+  ExpressionAttributeValues?: Record<string, AttributeValue>;
+  ReturnConsumedCapacity?: ReturnConsumedCapacity;
+  ReturnItemCollectionMetrics?: ReturnItemCollectionMetrics;
+  ReturnValues?: ReturnValue;
+  ReturnValuesOnConditionCheckFailure?: ReturnValuesOnConditionCheckFailure;
+  UpdateExpression?: string;
+};
+
+export const update = (): UpdateItemInput => {
+  const empty = {
+    Key: undefined,
+    TableName: undefined,
   };
+  return empty;
 };
